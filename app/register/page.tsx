980e2422cf743f1761378abee9fd808429f58c6c
pachara <pachared.amr12@gmail.com>
2026-02-23 01:09:39 +0700

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,10 @@
-import Register from "@/auth/Register";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Register = dynamic(() => import("@/auth/Register"), {
+    ssr: false,
+});
 
 export default function Page() {
     return <Register />;
