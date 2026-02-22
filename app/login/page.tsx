0502e9962aa00bba14c8b25e325896c3ef0ed635
pachara <pachared.amr12@gmail.com>
2026-02-23 01:09:32 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,10 @@
-import Login from "@/auth/Login";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Login = dynamic(() => import("@/auth/Login"), {
+    ssr: false,
+});
 
 export default function Page() {
     return <Login />;
