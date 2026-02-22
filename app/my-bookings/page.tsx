da7aef8ba73a55b2589e65f225288fdd2b8c734a
pachara <pachared.amr12@gmail.com>
2026-02-23 01:09:34 +0700

    Update page.tsx

--- app/my-bookings/page.tsx
+++ app/my-bookings/page.tsx
@@ -1,4 +1,11 @@
-import MyBookingsPage from "@/components/pages/MyBookingsPage";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const MyBookingsPage = dynamic(
+    () => import("@/components/pages/MyBookingsPage"),
+    { ssr: false }
+);
 
 export default function Page() {
     return <MyBookingsPage />;
