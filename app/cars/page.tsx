d096999cfda0ac1e319ddc3b6acf97fcb9c60de0
pachara <pachared.amr12@gmail.com>
2026-02-23 01:09:22 +0700

    Update page.tsx

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,4 +1,10 @@
-import CarsPage from "@/components/pages/CarsPage";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CarsPage = dynamic(() => import("@/components/pages/CarsPage"), {
+    ssr: false,
+});
 
 export default function Page() {
     return <CarsPage />;
