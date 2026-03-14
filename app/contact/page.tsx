978c2617dcd7adc7ad99d84d4623875696bc84f6
pachara <pachared.amr12@gmail.com>
2026-03-15 00:08:25 +0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,11 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const ContactPage = dynamic(
-  () => import("@/src/components/pages/ContactPage"),
-  {
-    ssr: false,
-  }
-);
+import { Suspense } from "react";
+import ContactPage from "@/src/components/pages/ContactPage";
+import ContactPageSkeleton from "@/src/components/contact/ContactPageSkeleton";
 
 export default function Page() {
-  return <ContactPage />;
+  return (
+    <Suspense fallback={<ContactPageSkeleton />}>
+      <ContactPage />
+    </Suspense>
+  );
 }
